ddee6ce1390e1e9895db6e412680a158375060f3
zhorton <zak.horton@vml.com>
2020-06-28 20:53:07 -0500

    Lesson 7 - Route Examples Added and composer.json php7.4 udpated to remove php storm red lines that aren't true

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', function () {
-    return view('home', request()->input());
-});
+Route::view('/', 'welcome');
+
+Route::get('/home/{id}', fn ($id) => [
+    'id' => $id
+])->where('id', '[0-9]+');
+
+
