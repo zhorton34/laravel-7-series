5f22114888c972b1a6d61aa79f5b43599a636819
zhorton <zak.horton@vml.com>
2020-06-30 10:00:32 -0500

    Cleaned up comments in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,42 +6,32 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
+    /*
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         return \DB::table('posts')->get();
     }
 
-    /**
+    /*
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
         return ['view' => 'with a form to create a post'];
     }
 
-    /**
+    /*
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //
     }
 
-    /**
+    /*
      * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
@@ -50,34 +40,24 @@ class PostController extends Controller
         return ['show' => $posts[$id]];
     }
 
-    /**
+    /*
      * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
         return ['edit' => 'form for post with an id of ' . $id];
     }
 
-    /**
+    /*
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
+    /*
      * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
